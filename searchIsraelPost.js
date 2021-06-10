532c1dddda19b9105ea7455f9ff54a5056175d0c
Tomer Gil <tomgi859@gmail.com>
2021-06-11 00:27:00 +0300

    Started handle the cookie header.
    - Changed httpHeaders.txt to JSON format (have not updated the file
        extension yet).
    - Added and fixed a few debug console logs.
    - Added a few constants for better control of the program.
    
    Currently I'm focusing on avoiding the captcha test, rather than
    solving it.
    I'm doing it by:
    1) Looking for "set-cookie" header in the server response.
    2) Updating the request headers object for next time (though currently
        not applying those headers).
    3) Updating httpRequest.txt.
    
    Remember I'm reading from httpHeaders.txt and updating it, but I don't
    actually update the request headers according to it (by commenting the
    line responsible for that).

--- searchIsraelPost.js
+++ searchIsraelPost.js
@@ -5,7 +5,8 @@ const path = require("path");
 const LOCATE_ZIPCODE_URL = "https://israelpost.co.il/%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99%D7%9D/%D7%90%D7%99%D7%AA%D7%95%D7%A8-%D7%9E%D7%99%D7%A7%D7%95%D7%93/";
 const IS_RANDOM_USER_AGENT = false;
 const IS_REFERER_HEADER = true;
-const IS_COOKIE_HEADER = false;
+const IS_COOKIE_HEADER = true;
+const httpHeadersPath = "httpHeaders.txt";
 
 const searchIsraelPost = async function(autoComplete) {
     let launchOptions = {
@@ -13,14 +14,14 @@ const searchIsraelPost = async function(autoComplete) {
             TZ: "UTC+3",
             ...process.env
         },
-        headless: false
+        // headless: false
     }
     const browser = await puppeteer.launch(launchOptions);
 
     const page = await browser.newPage();
 
     let httpHeaders = await new Promise(function(resolve, reject) {
-        fs.readFile(path.join(__dirname, "httpHeaders.txt"), {encoding: "utf-8"}, function(err, data) {
+        fs.readFile(path.join(__dirname, httpHeadersPath), {encoding: "utf-8"}, function(err, data) {
             if(err) {
                 reject("Error:\n" + err);
             } else {
@@ -28,15 +29,7 @@ const searchIsraelPost = async function(autoComplete) {
             }
         });
     });
-    httpHeaders = httpHeaders.split("\r\n").reduce(function(obj, currentHeader) {
-        let [name, value] = currentHeader.split(": ");
-        // hasOwnProperty is case-sensitive, so make sure all headers' names and values are lower-case.
-        [name, value] = [name, value].map(function(value) {
-            return value.toLowerCase();
-        });
-        obj[name] = value;
-        return obj;
-    }, {});
+    httpHeaders = JSON.parse(httpHeaders);
 
     // Make sure the User-Agent header is random each request.
     if(IS_RANDOM_USER_AGENT) {
@@ -62,7 +55,7 @@ const searchIsraelPost = async function(autoComplete) {
         }
     }
 
-    await page.setExtraHTTPHeaders(httpHeaders);
+    // await page.setExtraHTTPHeaders(httpHeaders);
 
     await page.setRequestInterception(true);
     page.on("request", request => {
@@ -89,14 +82,40 @@ const searchIsraelPost = async function(autoComplete) {
 
     const newestFile_number = parseInt(/[0-9]+/.exec(files[files.length - 1])[0]) + 1;
 
-    await page.goto(LOCATE_ZIPCODE_URL);
+    var israelPostResponse = await page.goto(LOCATE_ZIPCODE_URL);
+    if(israelPostResponse._headers.hasOwnProperty("set-cookie") && israelPostResponse._headers["set-cookie"] !== httpHeaders.cookie) {
+        // Update cookie header
+        let setCookieHeader = israelPostResponse._headers["set-cookie"].replace(/\/\n/g, "").replace(/PHPSESSID=[^;]*; /, "");
+        httpHeaders.cookie = setCookieHeader;
+        // Update httpHeaders.txt
+        try {
+            let isHeadersFileUpdated = await new Promise(function(resolve, reject) {
+                fs.writeFile(httpHeadersPath, JSON.stringify(httpHeaders), function(err) {
+                    if(err) {
+                        reject(err);
+                    } else {
+                        resolve(true);
+                    }
+                });
+            });
+        } catch(e) {
+            console.log(e);
+        }
+        try {
+            await page.setExtraHTTPHeaders(httpHeaders);
+        } catch(e) {
+            console.error(e);
+        }
+        israelPostResponse = await page.goto(LOCATE_ZIPCODE_URL);
+    }
     await page.screenshot({path: `example${newestFile_number}.png`});
 
     var isCityInputFound = true;
     try {
-        await page.click("body > div.container > div:nth-child(2) > div.captcha-mid > form > center > input");
         await page.click("#recaptcha-anchor > div.recaptcha-checkbox-checkmark");
-        console.log("Capatcha found.");
+        console.log("Capatcha solved.");
+        await page.click("body > div.container > div:nth-child(2) > div.captcha-mid > form > center > input");
+        console.log("Capatcha submitted.");
     } catch(e) {
         console.error(e);
     } finally {
